38d168044519b6053b162249718576d615a7049c
Hancong Zhang <daniel94126@hotmail.com>
2020-12-18 22:57:41 -0500

    FINAL Initial project

---
+++ Assets/_Scenes/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+        Cursor.visible = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
+ 
